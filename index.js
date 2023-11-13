47f1447f47ea0b6c967380988c68066d4f7d76c5
saipratap1218 <144958222+saipratap1218@users.noreply.github.com>
2023-11-13 07:42:27 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const element = (
+  <div className="congrats-card-container">
+    <h1 className="heading">Congratulations</h1>
+    <div className="card">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+        className="profile-image"
+      />
+      <h1 className="card-title">Pratap Reddy</h1>
+      <p className="card-description">
+        Sri Sai Institute of Technology and Science
+      </p>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+        className="watch-image"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
